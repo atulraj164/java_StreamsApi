b53d015b574ceb593eec74477992c688b65c56ac
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-04-15 22:47:48 +0530

    implement Java Optional for null-safe handling

---
+++ Optionals.java
@@ -0,0 +1,16 @@
+package streamApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+//optioinals special variables take generics 
+//used to avoid null pointer exception if no value or null alue returned by the stream
+public class Optionals {
+ public static void main(String [] args) {
+	 List<String> names=Arrays.asList("ram","sham","ghnsam");
+	 String can="ram";
+	Optional<String> str=names.stream().filter(n->n.equals(can)).findFirst();
+	System.out.println(str.get());
+}
+}
