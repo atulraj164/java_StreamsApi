826ff03ea928ca15d55a884cf52108688356a74b
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-04-15 23:43:11 +0530

    Add files via upload

---
+++ Method_Reffrence.java
@@ -0,0 +1,20 @@
+package Functional_programing;
+
+import java.util.Arrays;
+import java.util.List;
+
+//method REffrence used to implement functiona; style programing where we can call method inside method
+//A method reference is a shorthand way of writing a lambda expression that calls a method.
+//ClassName::methodName
+public class Method_Reffrence {
+  public static void main(String [] args) {
+	  
+	  List<String>numsSmall=Arrays.asList("ram","shyam");
+	  List<String>numsCap=numsSmall.stream().map(n->n.toUpperCase()).toList();
+	  
+	  //or we can use ,method reffrence
+	  List<String>numsCap1=numsSmall.stream().map(String::toUpperCase).toList();
+	  //just method and the class it belongs to
+	  
+  }
+} 
