fc72805dd9f5618f5af478ec6438866d7815251c
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-04-16 22:32:59 +0530

    Add files via upload

--- Method_Reffrence.java
+++ Method_Reffrence.java
@@ -1,9 +1,10 @@
 package Functional_programing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//method REffrence used to implement functiona; style programing where we can call method inside method
+//method REffrence used to implement functional; style programing where we can call method inside method
 //A method reference is a shorthand way of writing a lambda expression that calls a method.
 //ClassName::methodName
 public class Method_Reffrence {
@@ -14,7 +15,18 @@ public class Method_Reffrence {
 	  
 	  //or we can use ,method reffrence
 	  List<String>numsCap1=numsSmall.stream().map(String::toUpperCase).toList();
+	  
 	  //just method and the class it belongs to
 	  
+	  List<Students>students=new ArrayList<>();
+	  List<String>std=Arrays.asList("par","akns");
+	  
+	  //copy all names int students list by creating new object
+	  
+	  	//students=std.stream().map(n->new Students(n)).toList();
+	  	//or using reffrence
+	  students=std.stream().map(Students::new).toList();
+	  
+	  	
   }
 } 
